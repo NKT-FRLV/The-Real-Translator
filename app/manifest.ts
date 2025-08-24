1acc1bee7543115f726f90ca87643ea3d41eb790
FROL <nkt.frlv7@yandex.ru>
2025-08-24 02:00:30 +0200

    PWA settings fixed

--- app/manifest.ts
+++ app/manifest.ts
@@ -9,7 +9,7 @@ export default function manifest(): MetadataRoute.Manifest {
 		start_url: "/",
 		display: "standalone",
 		background_color: "#ffffff",
-		theme_color: "#0ea5e9",
+		theme_color: "#ffffff",
 		icons: [
 			{
 				src: "/icon-192.png",
